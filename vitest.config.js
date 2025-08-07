9966d2a7355211832f0dc193d99dc7b91919eb71
Nijk <1615460+nijk@users.noreply.github.com>
2025-08-08 00:07:57 +0100

    change pool setting to 'threads'

--- vitest.config.js
+++ vitest.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
         functions: 100
       }
     },
-    pool: 'forks',
+    pool: 'threads',
     setupFiles: ['.vite/mongo-memory-server.js', '.vite/setup-files.js']
   }
 })
