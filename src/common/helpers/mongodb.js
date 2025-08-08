f98bdd84fecc7a10d5d72ab8e7aad69a1988ed79
Nijk <1615460+nijk@users.noreply.github.com>
2025-08-08 01:48:41 +0100

    reintroduce indexes but remove timeout: 0 from mongodb.test

--- src/common/helpers/mongodb.js
+++ src/common/helpers/mongodb.js
@@ -48,5 +48,5 @@ async function createIndexes(db) {
   await db.collection('mongo-locks').createIndex({ id: 1 })
 
   // Example of how to create a mongodb index. Remove as required
-  // await db.collection('example-data').createIndex({ id: 1 })
+  await db.collection('example-data').createIndex({ id: 1 })
 }
