1426e66656ceba4ab647e3655b9d5c5456fc95e9
Mohammed Debashi <mohammed.debashi@defra.gov.uk>
2025-08-13 01:17:55 +0100

    PAE-120-investigate-dxt-api-email-limitation

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,12 @@
 import { health } from '../routes/health.js'
 import { example } from '../routes/example.js'
 import { notify } from '../routes/notify.js'
-
+import { testEndpointDXT } from '../routes/testEndpointDXT.js'
 const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
-      server.route([health, notify, ...example])
+      server.route([health, notify, testEndpointDXT, ...example])
     }
   }
 }
