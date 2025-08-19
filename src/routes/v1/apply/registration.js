048f8e00e147603d1545378ee720a1810340d236
Mohammed Debashi <mohammed.debashi@defra.gov.uk>
2025-08-19 17:21:28 +0100

    removing joi and other commits

--- src/routes/v1/apply/registration.js
+++ src/routes/v1/apply/registration.js
@@ -1,11 +1,10 @@
+import Boom from '@hapi/boom'
 import { createLogger } from '../../../common/helpers/logging/logger.js'
 import {
   LOGGING_EVENT_ACTIONS,
   LOGGING_EVENT_CATEGORIES
 } from '../../../common/enums/event.js'
-import Joi from 'joi'
 import { HTTP_STATUS } from '../../../common/constants/http-status-codes.js'
-import { badRequestResponseSchema } from '../../../schemas/bad-request-response-schema.js'
 
 /*
  * Registration endpoint
@@ -18,27 +17,11 @@ const registration = {
   path: '/v1/apply/registration',
   options: {
     validate: {
-      payload: Joi.object().unknown(true).required().messages({
-        'object.base': 'Invalid payload — must be a JSON object',
-        'any.required': 'Invalid payload — required'
-      })
-    },
-    plugins: {
-      'hapi-swagger': {
-        responses: {
-          [HTTP_STATUS.OK]: {
-            description: 'Registration submission successful',
-            schema: Joi.object({
-              message: Joi.string().example(
-                'Registration submission successful'
-              )
-            })
-          },
-          [HTTP_STATUS.BAD_REQUEST]: {
-            description: 'Invalid payload',
-            schema: badRequestResponseSchema
-          }
+      payload: (value, _options) => {
+        if (!value || typeof value !== 'object') {
+          throw Boom.badRequest('Invalid payload — must be JSON object')
         }
+        return value
       }
     }
   },
