16ccd138b61179b2d0f142b210fb0531c8475be0
Nijk <1615460+nijk@users.noreply.github.com>
2025-08-27 15:25:55 +0100

    PAE-198: remove /send-email endpoint

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,11 @@
 import { health } from '../routes/health.js'
-import { notify } from '../routes/notify.js'
 import { apply } from '../routes/v1/apply/index.js'
 
 const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
-      server.route([health, notify, ...apply])
+      server.route([health, ...apply])
     }
   }
 }
