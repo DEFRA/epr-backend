2afa4cba6a56180c46fc4854d4ea9f360bd57bf0
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-10-23 16:24:42 +0100

    feat(organisations): add findRegistrationById to port definition

--- src/repositories/organisations/port.js
+++ src/repositories/organisations/port.js
@@ -3,6 +3,7 @@
  * @property {(organisation: Object) => Promise<void>} insert
  * @property {() => Promise<Object[]>} findAll
  * @property {(id: string) => Promise<Object|null>} findById
+ * @property {(organisationId: string, registrationId: string) => Promise<Object|null>} findRegistrationById
  */
 
 /**
