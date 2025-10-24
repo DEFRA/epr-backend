e7fe31a725d868e4ff45f3a5f6d20bfa4b1a766b
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-10-24 12:22:07 +0100

    remove babel & hence .cjs support

--- eslint.config.js
+++ eslint.config.js
@@ -20,18 +20,6 @@ export default [
   ...ns,
   nodePlugin.configs['flat/recommended-module'],
   {
-    files: ['**/*.cjs'],
-    languageOptions: {
-      sourceType: 'commonjs',
-      globals: {
-        module: 'readonly',
-        require: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly'
-      }
-    }
-  },
-  {
     files: ['.vite/**/*.js', '**/*.contract.js'],
     rules: {
       'n/no-unpublished-import': 'off'
