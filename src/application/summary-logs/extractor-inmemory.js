e5d5958dec6e099057a2e4cfb2ac9ac626713d61
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-10-24 12:55:01 +0100

    feat: add in-memory SummaryLogExtractor implementation

--- src/application/summary-logs/extractor-inmemory.js
+++ src/application/summary-logs/extractor-inmemory.js
@@ -1,14 +1,30 @@
+/** @typedef {import('#domain/summary-logs/extractor/port.js').SummaryLogExtractor} SummaryLogExtractor */
+/** @typedef {import('#domain/summary-logs/extractor/port.js').ParsedSummaryLog} ParsedSummaryLog */
+/** @typedef {import('#domain/summary-logs/model.js').SummaryLog} SummaryLog */
+
 /**
  * Creates an in-memory summary log extractor for testing
- *
- * @param {Object} [options]
- * @param {Object} [options.parsed] - The parsed data to return
- * @returns {{ extract: (summaryLog: any) => Promise<Object> }}
+ * @param {Object.<string, ParsedSummaryLog>} testDataMap - Map of file IDs to parsed data
+ * @returns {SummaryLogExtractor}
  */
-export const createSummaryLogExtractor = (options = {}) => {
-  const { parsed = { meta: {}, data: {} } } = options
-
+export const createInMemorySummaryLogExtractor = (testDataMap) => {
   return {
-    extract: async (_summaryLog) => parsed
+    /**
+     * @param {SummaryLog} summaryLog
+     * @returns {Promise<ParsedSummaryLog>}
+     */
+    extract: async (summaryLog) => {
+      const {
+        file: { id: fileId }
+      } = summaryLog
+
+      if (!testDataMap[fileId]) {
+        throw new Error(
+          'Something went wrong while retrieving your file upload'
+        )
+      }
+
+      return testDataMap[fileId]
+    }
   }
 }
