38f8ba3b8edd645e4d2460200e3018ae0bbceee0
Carles Andres <carlos.andres.c@gmail.com>
2025-10-29 08:41:43 +0000

    Add new PUT /organisations/{id} endpoint
    WIP Scaffolded patch organisations endpoint
    WIP Shape-up request and added some custom validation
    WIP remove console.logs
    WIP Remove typo
    WIP Sanitised payload and added some tests
    WIP Small refactor
    WIP Add tests for invalid payload
    WIP Added test for validation errors in response
    WIP Change organisations update endpoint to PUT method

---
+++ src/routes/v1/organisations/put-by-id.js
@@ -0,0 +1,61 @@
+import Boom from '@hapi/boom'
+import { StatusCodes } from 'http-status-codes'
+/** @typedef {import('#repositories/organisations/port.js').OrganisationsRepository} OrganisationsRepository */
+
+export const organisationsPutByIdPath = '/v1/organisations/{id}'
+
+const validateMyPayload = (payload) => {
+  if (typeof payload.version !== 'number') {
+    throw Boom.badRequest('Payload must include a numeric version field')
+  }
+
+  if (
+    typeof payload.updateFragment !== 'object' ||
+    payload.updateFragment === null
+  ) {
+    throw Boom.badRequest('Payload must include an updateFragment object')
+  }
+
+  return payload
+}
+
+export const organisationsPutById = {
+  method: 'PUT',
+  path: organisationsPutByIdPath,
+  options: {
+    validate: {
+      payload: validateMyPayload
+    }
+  },
+
+  /**
+   * @param {import('#common/hapi-types.js').HapiRequest & {organisationsRepository: OrganisationsRepository, params: { orgId: string }}} request
+   * @param {Object} h - Hapi response toolkit
+   */
+  handler: async (request, h) => {
+    const { organisationsRepository } = request
+
+    const id = request.params.id.trim()
+
+    if (!id) {
+      throw Boom.notFound('Organisation not found')
+    }
+
+    const { version, updateFragment } = request.payload
+
+    const {
+      version: _v,
+      id: _,
+      schemaVersion: _s,
+      ...sanitisedFragment
+    } = updateFragment
+
+    try {
+      await organisationsRepository.update(id, version, sanitisedFragment)
+    } catch (error) {
+      throw Boom.boomify(error)
+    }
+
+    return h.response(null).code(StatusCodes.NO_CONTENT)
+  }
+}
