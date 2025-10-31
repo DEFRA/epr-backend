1bfbf5d4e783afcf9a715a7b5f271c0c3e9364a3
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-10-31 16:23:33 +0000

    docs: add types for Registration and Accreditation

--- src/repositories/organisations/port.js
+++ src/repositories/organisations/port.js
@@ -1,10 +1,29 @@
 /**
+ * @typedef {Object} Accreditation
+ * @property {string} id
+ * @property {number} accreditationNumber
+ * @property {string} material
+ * @property {string} wasteProcessingType
+ */
+
+/**
+ * @typedef {Object} Registration
+ * @property {string} id
+ * @property {string} orgName
+ * @property {string} material
+ * @property {string} wasteProcessingType
+ * @property {string} wasteRegistrationNumber
+ * @property {string} [accreditationId]
+ * @property {Accreditation} [accreditation] - Hydrated accreditation object when accreditationId exists
+ */
+
+/**
  * @typedef {Object} OrganisationsRepository
  * @property {(organisation: Object) => Promise<void>} insert
  * @property {(id: string, version: number, updates: Object) => Promise<void>} update
  * @property {() => Promise<Object[]>} findAll
  * @property {(id: string) => Promise<Object|null>} findById
- * @property {(organisationId: string, registrationId: string) => Promise<Object|null>} findRegistrationById
+ * @property {(organisationId: string, registrationId: string) => Promise<Registration|null>} findRegistrationById
  */
 
 /**
