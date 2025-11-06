78a0b6e22fe3c7d07102af5557cd035b7fbaca55
Carles Andres <carlos.andres.c@gmail.com>
2025-11-06 09:36:48 +0000

    WIP Bypass mongodb download in unit tests

--- .vite/setup-msw.js
+++ .vite/setup-msw.js
@@ -1,11 +1,18 @@
 import { setupServer } from 'msw/node'
 import { beforeAll, afterEach, afterAll } from 'vitest'
 
+import { http } from 'msw'
 export { http, HttpResponse, delay } from 'msw'
 
 export const handlers = []
+const mongoDownloadPassthrough = http.get(
+  'https://fastdl.mongodb.org/*',
+  (req, res, ctx) => {
+    return req.passthrough()
+  }
+)
 
-export const server = setupServer(...handlers)
+export const server = setupServer(...handlers, mongoDownloadPassthrough)
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'warn' })
