4b6b97cdc2cca378ce3a49e69379b10a8303e1ae
Senthil Kumar Arumugam <16401283+senthilinfy@users.noreply.github.com>
2025-11-06 09:58:55 +0000

    PAE-260 LDM updates for new fields to be supplied by regulators (#282)
    Co-authored-by: Senthil Kumar Arumugam <senthil.arumugam@equalexperts.com>

--- src/repositories/organisations/schema.js
+++ src/repositories/organisations/schema.js
@@ -243,6 +243,9 @@ const registrationSchema = Joi.object({
       STATUS.ARCHIVED
     )
     .forbidden(),
+  registrationNumber: Joi.string().optional(),
+  validFrom: Joi.date().optional(),
+  validTo: Joi.date().optional(),
   formSubmissionTime: Joi.date().required(),
   submittedToRegulator: Joi.string()
     .valid(REGULATOR.EA, REGULATOR.NRW, REGULATOR.SEPA, REGULATOR.NIEA)
@@ -291,7 +294,7 @@ const registrationSchema = Joi.object({
 
 const accreditationSchema = Joi.object({
   id: idSchema,
-  accreditationNumber: Joi.number().optional(),
+  accreditationNumber: Joi.string().optional(),
   status: Joi.string()
     .valid(
       STATUS.CREATED,
@@ -301,6 +304,8 @@ const accreditationSchema = Joi.object({
       STATUS.ARCHIVED
     )
     .forbidden(),
+  validFrom: Joi.date().optional(),
+  validTo: Joi.date().optional(),
   formSubmissionTime: Joi.date().required(),
   submittedToRegulator: Joi.string()
     .valid(REGULATOR.EA, REGULATOR.NRW, REGULATOR.SEPA, REGULATOR.NIEA)
