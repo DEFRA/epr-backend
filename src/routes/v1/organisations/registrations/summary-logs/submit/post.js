43931494e7df5979836d128c6ddcc824ccb4f811
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-11-18 16:29:40 +0000

    PAE-474: Address PR comments on summary log endpoints (#350)

--- src/routes/v1/organisations/registrations/summary-logs/submit/post.js
+++ src/routes/v1/organisations/registrations/summary-logs/submit/post.js
@@ -18,6 +18,7 @@ export const summaryLogsSubmit = {
   method: 'POST',
   path: summaryLogsSubmitPath,
   options: {
+    // Authentication will be implemented in follow-up story
     auth: false,
     response: {
       schema: summaryLogResponseSchema
