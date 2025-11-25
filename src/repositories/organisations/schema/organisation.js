9d4ee62e457dce950e113ad36040ba0dc23d3787
David Lusby <1230979+loveboat@users.noreply.github.com>
2025-11-25 11:42:30 +0000

    rename to linked-defra-organisation in organisation

--- src/repositories/organisations/schema/organisation.js
+++ src/repositories/organisations/schema/organisation.js
@@ -13,8 +13,8 @@ import {
 import {
   collatedUserSchema,
   companyDetailsSchema,
-  defraIdSchema,
   idSchema,
+  linkedDefraOrganisationSchema,
   partnershipSchema,
   userSchema
 } from './base.js'
@@ -24,8 +24,32 @@ export { idSchema, statusHistoryItemSchema } from './base.js'
 export { registrationSchema } from './registration.js'
 
 export const organisationInsertSchema = Joi.object({
+  accreditations: Joi.array().items(accreditationSchema).optional(),
+  businessType: Joi.string()
+    .valid(
+      BUSINESS_TYPE.INDIVIDUAL,
+      BUSINESS_TYPE.UNINCORPORATED,
+      BUSINESS_TYPE.PARTNERSHIP
+    )
+    .optional(),
+  companyDetails: companyDetailsSchema.required(),
+  formSubmissionTime: Joi.date().iso().required(),
   id: idSchema,
+  linkedDefraOrganisation: linkedDefraOrganisationSchema.optional(),
+  managementContactDetails: userSchema.optional(),
   orgId: Joi.number().required(),
+  partnership: partnershipSchema.optional(),
+  registrations: Joi.array().items(registrationSchema).optional(),
+  reprocessingNations: Joi.array()
+    .items(
+      Joi.string().valid(
+        NATION.ENGLAND,
+        NATION.WALES,
+        NATION.SCOTLAND,
+        NATION.NORTHERN_IRELAND
+      )
+    )
+    .optional(),
   status: Joi.string()
     .valid(
       STATUS.CREATED,
@@ -35,6 +59,11 @@ export const organisationInsertSchema = Joi.object({
       STATUS.ARCHIVED
     )
     .forbidden(),
+  submittedToRegulator: Joi.string()
+    .valid(REGULATOR.EA, REGULATOR.NRW, REGULATOR.SEPA, REGULATOR.NIEA)
+    .required(),
+  submitterContactDetails: userSchema.required(),
+  users: Joi.array().items(collatedUserSchema).optional(),
   wasteProcessingTypes: Joi.array()
     .items(
       Joi.string().valid(
@@ -46,36 +75,7 @@ export const organisationInsertSchema = Joi.object({
     .required()
     .messages({
       'array.min': 'At least one waste processing type is required'
-    }),
-  reprocessingNations: Joi.array()
-    .items(
-      Joi.string().valid(
-        NATION.ENGLAND,
-        NATION.WALES,
-        NATION.SCOTLAND,
-        NATION.NORTHERN_IRELAND
-      )
-    )
-    .optional(),
-  businessType: Joi.string()
-    .valid(
-      BUSINESS_TYPE.INDIVIDUAL,
-      BUSINESS_TYPE.UNINCORPORATED,
-      BUSINESS_TYPE.PARTNERSHIP
-    )
-    .optional(),
-  companyDetails: companyDetailsSchema.required(),
-  partnership: partnershipSchema.optional(),
-  submitterContactDetails: userSchema.required(),
-  managementContactDetails: userSchema.optional(),
-  defraId: defraIdSchema.optional(),
-  users: Joi.array().items(collatedUserSchema).optional(),
-  formSubmissionTime: Joi.date().iso().required(),
-  submittedToRegulator: Joi.string()
-    .valid(REGULATOR.EA, REGULATOR.NRW, REGULATOR.SEPA, REGULATOR.NIEA)
-    .required(),
-  registrations: Joi.array().items(registrationSchema).optional(),
-  accreditations: Joi.array().items(accreditationSchema).optional()
+    })
 })
 
 const NON_UPDATABLE_FIELDS = ['id']
