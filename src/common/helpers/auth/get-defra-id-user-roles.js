6512afd89d28849ac1d47f87421fa13bb29ac1d0
Carles Andres <carlos.andres.c@gmail.com>
2025-11-26 17:39:48 +0000

    WIP Fix function name

--- src/common/helpers/auth/get-defra-id-user-roles.js
+++ src/common/helpers/auth/get-defra-id-user-roles.js
@@ -1,10 +1,12 @@
 import { ROLES } from '#common/helpers/auth/constants.js'
 import { isInitialUser } from '#common/helpers/auth/roles/helpers.js'
 
-export function getDefraIdRoles(linkedEprOrg, tokenPayload) {
+export function getDefraIdUserRoles(linkedEprOrg, tokenPayload) {
   const { email } = tokenPayload
 
   if (isInitialUser(linkedEprOrg, email)) {
     return [ROLES.initialUser]
   }
+
+  return []
 }
