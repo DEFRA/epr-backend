e00cb6d7cd5ff7e47308eb7b36ea7aa67941fba3
Carles Andres <carlos.andres.c@gmail.com>
2025-12-02 16:48:25 +0000

    WIP Add types for token payloads

---
+++ src/common/helpers/auth/types.js
@@ -0,0 +1,57 @@
+/**
+ * Entra ID (Azure Active Directory) token payload
+ *
+ * Used for Admin UI authentication and service maintainer access
+ *
+ * @typedef {{
+ *   id: string
+ *   email?: string
+ *   preferred_username?: string
+ *   iss: string
+ *   aud: string
+ *   sub?: string
+ *   oid?: string
+ *   exp: number
+ *   iat: number
+ *   nbf?: number
+ * }} EntraIdTokenPayload
+ */
+
+/**
+ * Defra ID token payload
+ *
+ * Used for Frontend application authentication and organization-based access control
+ *
+ * @typedef {{
+ *   id: string
+ *   email: string
+ *   firstName: string
+ *   lastName: string
+ *   currentRelationshipId: string
+ *   relationships: string[]
+ *   iss: string
+ *   aud: string
+ *   exp: number
+ *   iat: number
+ *   nbf?: number
+ * }} DefraIdTokenPayload
+ */
+
+/**
+ * Union type representing any valid token payload from either identity provider
+ *
+ * @typedef {EntraIdTokenPayload | DefraIdTokenPayload} TokenPayload
+ */
+
+/**
+ * Parsed organization data extracted from Defra ID token relationships
+ *
+ * @typedef {{
+ *   defraIdRelationshipId: string
+ *   defraIdOrgId: string
+ *   defraIdOrgName: string
+ *   isCurrent: boolean
+ * }} DefraIdRelationship
+ */
+
+export {} // NOSONAR: javascript:S7787 - Required to make this file a module for JSDoc @import
