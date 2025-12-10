9bcfc04bca7e982202ca648ca61a6852bbaec4d7
Duncan Sommerville <233909619+sommerville-ee-defra@users.noreply.github.com>
2025-12-10 14:15:22 +0000

    PAE-607: Introduces in-sheet validations for Reprocessor Input summary log (#488)

--- src/domain/summary-logs/table-schemas/shared/joi-messages.test.js
+++ src/domain/summary-logs/table-schemas/shared/joi-messages.test.js
@@ -15,7 +15,7 @@ describe('joi-messages', () => {
       expect(MESSAGES.MUST_BE_GREATER_THAN_ZERO).toBe('must be greater than 0')
       expect(MESSAGES.MUST_BE_AT_LEAST_ZERO).toBe('must be at least 0')
       expect(MESSAGES.MUST_BE_LESS_THAN_ONE).toBe('must be less than 1')
-      expect(MESSAGES.MUST_BE_AT_MOST_ONE).toBe('must be at most 1')
+      expect(MESSAGES.MUST_BE_AT_MOST_1).toBe('must be at most 1')
       expect(MESSAGES.MUST_BE_AT_MOST_1000).toBe('must be at most 1000')
       expect(MESSAGES.MUST_BE_YES_OR_NO).toBe('must be Yes or No')
     })
