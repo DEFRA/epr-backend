5e008c80ec66ada134b6f1b25fdcf06c5c1d5944
Carles Andres <carlos.andres.c@gmail.com>
2025-12-11 13:01:00 +0000

    PAE-675: New test helpers

---
+++ .vite/helpers/build-approved-org.js
@@ -0,0 +1,52 @@
+import { buildOrganisation } from '#repositories/organisations/contract/test-data.js'
+import { STATUS } from '#domain/organisations/model.js'
+
+export async function buildApprovedOrg(organisationsRepository) {
+  const org = buildOrganisation()
+
+  const INITIAL_VERSION = 1
+
+  await organisationsRepository.insert(org)
+
+  const now = new Date()
+  const oneYearFromNow = new Date(
+    now.getFullYear() + 1,
+    now.getMonth(),
+    now.getDate()
+  )
+
+  // Only approve the first accreditation (which is already linked to the first registration)
+  const approvedAccreditations = [
+    {
+      ...org.accreditations[0],
+      status: STATUS.APPROVED,
+      accreditationNumber: org.accreditations[0].accreditationNumber || 'ACC1',
+      validFrom: now,
+      validTo: oneYearFromNow
+    }
+  ]
+
+  const approvedRegistrations = [
+    Object.assign({}, org.registrations[0], {
+      status: STATUS.APPROVED,
+      cbduNumber: org.registrations[0].cbduNumber || 'CBDU123456',
+      registrationNumber: 'REG1',
+      validFrom: now,
+      validTo: oneYearFromNow
+    })
+  ]
+
+  await organisationsRepository.update(org.id, INITIAL_VERSION, {
+    status: STATUS.APPROVED,
+    accreditations: approvedAccreditations,
+    registrations: approvedRegistrations
+  })
+
+  // TODO: For some reason, without this request tests fail
+  const updatedOrg = await organisationsRepository.findById(
+    org.id,
+    INITIAL_VERSION + 1
+  )
+
+  return updatedOrg
+}
