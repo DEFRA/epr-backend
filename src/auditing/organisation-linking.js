6d0ac2329412c9819af7b6c57048b419a1444836
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2026-01-23 14:09:02 +0000

    PAE-938 add auditing, system log and metric for organisation linking

---
+++ src/auditing/organisation-linking.js
@@ -0,0 +1,31 @@
+import { audit } from '@defra/cdp-auditing'
+import { extractUserDetails, recordSystemLog } from './helpers.js'
+
+/**
+ * @import {SystemLogsRepository} from '#repositories/system-logs/port.js'
+ */
+
+/**
+ * @param {import('#common/hapi-types.js').HapiRequest & {systemLogsRepository: SystemLogsRepository}} request
+ * @param {string} organisationId
+ * @param {{ id: string, name: string }} linkedDefraOrganisation
+ */
+async function auditOrganisationLinking(request, organisationId, { id, name }) {
+  const payload = {
+    event: {
+      category: 'entity',
+      subCategory: 'epr-organisations',
+      action: 'linked-to-defra-id-organisation'
+    },
+    context: {
+      organisationId,
+      linkedDefraOrganisation: { id, name }
+    },
+    user: extractUserDetails(request)
+  }
+
+  audit(payload)
+  await recordSystemLog(request, payload)
+}
+
+export { auditOrganisationLinking }
