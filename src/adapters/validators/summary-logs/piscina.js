b98ce2f95a73524f62e9e4c582838205cb9c891e
Tom Halley <2283407+tomhalley@users.noreply.github.com>
2026-02-02 12:58:36 +0000

    PAE-000: Fixed failing tests

--- src/adapters/validators/summary-logs/piscina.js
+++ src/adapters/validators/summary-logs/piscina.js
@@ -203,20 +203,18 @@ const runCommandInWorker = async (
       }
     })
 
-    if (!repository) {
-      throw new Error(`No repository provided to mark summary log as failed`)
-    }
-
-    switch (command) {
-      case SUMMARY_LOG_COMMAND.VALIDATE:
-        await markAsValidationFailed(summaryLogId, repository, logger)
-        break
-      case SUMMARY_LOG_COMMAND.SUBMIT:
-        await markAsSubmissionFailed(summaryLogId, repository, logger)
-        break
-      /* v8 ignore next 2 */
-      default:
-        throw new Error(`Unknown command: ${command}`)
+    if (repository) {
+      switch (command) {
+        case SUMMARY_LOG_COMMAND.VALIDATE:
+          await markAsValidationFailed(summaryLogId, repository, logger)
+          break
+        case SUMMARY_LOG_COMMAND.SUBMIT:
+          await markAsSubmissionFailed(summaryLogId, repository, logger)
+          break
+        /* v8 ignore next 2 */
+        default:
+          throw new Error(`Unknown command: ${command}`)
+      }
     }
   }
 }
