6df3b06747b3bbdc4271f8b2914f44edda318921
Philip Morris <philip@Philips-MacBook-Air.local>
2026-02-04 18:07:43 +0000

    split glass remelt and other

--- src/routes/v1/tonnage-monitoring/response.schema.js
+++ src/routes/v1/tonnage-monitoring/response.schema.js
@@ -1,15 +1,30 @@
 import Joi from 'joi'
-import { MATERIAL } from '#domain/organisations/model.js'
+import {
+  MATERIAL,
+  GLASS_RECYCLING_PROCESS
+} from '#domain/organisations/model.js'
+
+const tonnageMonitoringMaterials = Object.values(MATERIAL)
+  .filter((m) => m !== MATERIAL.GLASS)
+  .concat(Object.values(GLASS_RECYCLING_PROCESS))
 
 const materialTonnageSchema = Joi.object({
   material: Joi.string()
-    .valid(...Object.values(MATERIAL))
+    .valid(...tonnageMonitoringMaterials)
     .required(),
   totalTonnage: Joi.number().required()
 })
 
+const materialsExample = tonnageMonitoringMaterials.map((material) => ({
+  material,
+  totalTonnage: 0
+}))
+
 export const tonnageMonitoringResponseSchema = Joi.object({
   generatedAt: Joi.string().isoDate().required(),
-  materials: Joi.array().items(materialTonnageSchema).required(),
+  materials: Joi.array()
+    .items(materialTonnageSchema)
+    .required()
+    .example(materialsExample),
   total: Joi.number().required()
 })
