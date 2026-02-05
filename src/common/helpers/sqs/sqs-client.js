bafdd3efcd9bbfee2f24ebb48ea48d4c651ec7a5
Dan Sweeting <dansweeting@gmail.com>
2026-02-05 19:19:02 +0000

    Address Copilot review suggestions
    - Move user destructuring into submit case for explicit data flow
    - Simplify SQS config using spread operators for conditionals
    - Revert package-lock.json to match main

--- src/common/helpers/sqs/sqs-client.js
+++ src/common/helpers/sqs/sqs-client.js
@@ -4,15 +4,9 @@ import { SQSClient, GetQueueUrlCommand } from '@aws-sdk/client-sqs'
 
 export function createSqsClient({ region, endpoint, credentials = undefined }) {
   const config = {
-    region
-  }
-
-  if (endpoint) {
-    config.endpoint = endpoint
-  }
-
-  if (credentials) {
-    config.credentials = credentials
+    region,
+    ...(endpoint ? { endpoint } : {}),
+    ...(credentials ? { credentials } : {})
   }
 
   return new SQSClient(config)
