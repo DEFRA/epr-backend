94464fd3a11a303d8dd4340de6d2607c501af068
Philip Morris <philip@Philips-MacBook-Air.local>
2026-02-09 18:09:48 +0000

    make response less permissive

--- src/routes/v1/linked-organisations/get.js
+++ src/routes/v1/linked-organisations/get.js
@@ -5,6 +5,16 @@ import { linkedOrganisationsResponseSchema } from './response.schema.js'
 
 /** @typedef {import('#repositories/organisations/port.js').OrganisationsRepository} OrganisationsRepository */
 
+const toLinkedOrganisationSummary = (org) => ({
+  id: org.id,
+  orgId: org.orgId,
+  companyDetails: {
+    name: org.companyDetails.name
+  },
+  status: org.status,
+  linkedDefraOrganisation: org.linkedDefraOrganisation
+})
+
 export const linkedOrganisationsGetAllPath = '/v1/linked-organisations'
 
 export const linkedOrganisationsGetAll = {
@@ -34,6 +44,8 @@ export const linkedOrganisationsGetAll = {
     const organisations =
       await request.organisationsRepository.findAllLinked(filter)
 
-    return h.response(organisations).code(StatusCodes.OK)
+    return h
+      .response(organisations.map(toLinkedOrganisationSummary))
+      .code(StatusCodes.OK)
   }
 }
