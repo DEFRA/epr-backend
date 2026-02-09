1c332ef437f07ef5cc7ba47ab46b9d9a94044d5a
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-09 16:12:15 +0000

    allow tests to run in parallel

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     environment: 'node',
     clearMocks: true,
     hookTimeout: 60000,
-    fileParallelism: !process.env.CI,
+    fileParallelism: true,
     coverage: {
       provider: 'v8',
       reportsDirectory: './coverage',
