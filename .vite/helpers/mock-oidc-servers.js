1864ed386eafed45c7ba1aa08fa766732b02d71a
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-10 18:05:08 +0000

    add msw handler for aws imds

--- .vite/helpers/mock-oidc-servers.js
+++ .vite/helpers/mock-oidc-servers.js
@@ -1,11 +1,22 @@
+import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
 import { entraIdOidcHandlers } from './mock-entra-oidc.js'
 import { defraIdOidcHandlers } from './mock-defra-id-oidc.js'
+
+const awsImdsHandlers = [
+  http.put('http://169.254.169.254/latest/api/token', () => {
+    return new HttpResponse(null, { status: 403 })
+  })
+]
 
 /**
  * Create and configure MSW server for OIDC tests
  * @returns {import('msw/node').SetupServer}
  */
 export const createMockOidcServers = () => {
-  return setupServer(...entraIdOidcHandlers, ...defraIdOidcHandlers)
+  return setupServer(
+    ...entraIdOidcHandlers,
+    ...defraIdOidcHandlers,
+    ...awsImdsHandlers
+  )
 }
