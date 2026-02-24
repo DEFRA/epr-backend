8572c590778e3078ae51cf82a10447074bfa9bfa
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-24 17:26:38 +0000

    add to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import vitest from '@vitest/eslint-plugin'
 import nodePlugin from 'eslint-plugin-n'
 import neostandard from 'neostandard'
 
@@ -23,6 +24,16 @@ export default [
     files: ['.vite/**/*.js', '**/*.contract.js', 'benchmarks/**/*.js'],
     rules: {
       'n/no-unpublished-import': 'off'
+    }
+  },
+  {
+    files: ['**/*.test.js', '**/*.contract.js'],
+    plugins: { vitest },
+    rules: vitest.configs.recommended.rules,
+    settings: {
+      vitest: {
+        vitestImports: [/#vite\/fixtures\//]
+      }
     }
   },
   {
