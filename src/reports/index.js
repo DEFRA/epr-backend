3cf4a673f3b31be74c0cc2cf6bed9fc9f6b0c662
Duncan Sommerville <233909619+sommerville-ee-defra@users.noreply.github.com>
2026-03-11 11:08:28 +0000

    PAE-1186: Move reports to self-contained module following packaging-recycling-notes pattern

---
+++ src/reports/index.js
@@ -0,0 +1,10 @@
+/**
+ * Reports Module
+ *
+ * Contains domain logic and routes for reporting periods.
+ *
+ * @module reports
+ */
+
+// Route exports
+export { reportsGet } from './routes/get.js'
