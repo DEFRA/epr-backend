85d443786cdc00805b7e87fc759117c98404105c
Duncan Sommerville <233909619+sommerville-ee-defra@users.noreply.github.com>
2026-04-16 14:34:51 +0100

    PAE-1191: Address type issue

--- src/repositories/organisations/status.js
+++ src/repositories/organisations/status.js
@@ -1,9 +1,6 @@
-/** @import {Accreditation} from '#domain/organisations/accreditation.js' */
-/** @import {Organisation, RegAccStatus} from '#domain/organisations/model.js' */
-/** @import {Registration} from '#domain/organisations/registration.js' */
+/** @import {RegAccStatus} from '#domain/organisations/model.js' */
 
 /**
- * @param {Organisation|Registration|Accreditation} item
  * @returns {RegAccStatus}
  */
 export const getCurrentStatus = (item) => item.statusHistory.at(-1).status
