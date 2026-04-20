be9c60317b32c2fe2cd6fa5ff3694138ecdc5a58
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2026-04-20 11:51:50 +0100

    PAE-1373: Speed up admin overseas sites list query (#1081)

--- src/repositories/organisations/helpers.js
+++ src/repositories/organisations/helpers.js
@@ -372,7 +372,17 @@ const buildOrsAdminListBasePipeline = ({ registrationNumber }) => [
         {
           $match: {
             $expr: {
-              $eq: [{ $toString: '$_id' }, '$$overseasSiteId']
+              $eq: [
+                '$_id',
+                {
+                  $convert: {
+                    input: '$$overseasSiteId',
+                    to: 'objectId',
+                    onError: null,
+                    onNull: null
+                  }
+                }
+              ]
             }
           }
         }
