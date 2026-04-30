a3a67e2e2c4783b3cd434f2f282efe91c3253056
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-04-30 12:28:05 +0100

    PAE-1412: drop dead err serializer (ecsFormat bypasses pino serializers)

--- src/common/helpers/logging/logger-options.js
+++ src/common/helpers/logging/logger-options.js
@@ -2,8 +2,6 @@ import { config, isProductionEnvironment } from '#root/config.js'
 import { getTraceId } from '@defra/hapi-tracing'
 import { ecsFormat } from '@elastic/ecs-pino-format'
 
-import { errSerializer } from './err-serializer.js'
-
 const logConfig = config.get('log')
 const serviceName = config.get('serviceName')
 const serviceVersion = config.get('serviceVersion')
@@ -46,7 +44,6 @@ export const loggerOptions = {
   nesting: true,
   logEvents: ['onPostStart', 'onPostStop', 'response', 'request-error'],
   serializers: {
-    err: errSerializer,
     res: (res) => {
       if (!res) {
         return res
